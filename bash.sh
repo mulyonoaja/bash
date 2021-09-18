22c50d89f5c36eb92931d3f1a271c944cabb6e4e
mulyonoaja <60865079+mulyonoaja@users.noreply.github.com>
2021-09-19 02:54:08 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,1 @@
-echo "<h1><marquee>BUILD OTOMATIS KE $BUILD_NUMBER BELAJAR JENKINS BERSAMA MULYONO DI GCP, yuk</marquee></h1>" > index.html
+echo "<h1><marquee>BUILD OTOMATIS KE $BUILD_NUMBER BELAJAR JENKINS BERSAMA MULYONO DI GCP, yukkkkkk</marquee></h1>" > index.html
